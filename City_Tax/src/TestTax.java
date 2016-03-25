8d1ad52ca0370ec03b0335329770c4df4b01f76f
Makoveich <makoveich@ukr.net>
2016-03-25 16:15:41 +0200

    Revert "Revert "My first program added, it doesn't have SET part in Run.class""
    This reverts commit cd7094529259499f928f366d9c31a28e1602a50e.

--- City_Tax/src/TestTax.java
+++ City_Tax/src/TestTax.java
@@ -13,39 +13,7 @@ public class TestTax {
         TaxSumy simpleWorker3 = new TaxSumy(320, 170, 3000);
         simpleWorker3.setCity("Sumy");
         Tax[] array = {simpleWorker1, simpleWorker2, simpleWorker3};
-        for (Tax i : array) {
-            System.out.println(i.balance());
 
-        }
-        int i = 0;
-        Scanner scanner = new Scanner(System.in);
-        String askedCity = scanner.nextLine();
-
-        while (i < array.length) {
-            if (array[i].city.equals(askedCity)) {
-                System.out.println("For city " + askedCity + ":");
-                System.out.println(array[i].toString());
-                i++;
-                continue;
-            }
-            System.out.println("Element " + array[i].city + " was found.");
-            i++;
-        }
-
-        System.out.println("\n It was WHILE loop!!! \n");
-        i=0;
-
-        askedCity = args[1];
-
-        do {
-            if (array[i].city.equals(askedCity)) {
-                System.out.println("For city " + askedCity + ":");
-                System.out.println(array[i].toString());
-                i++;
-                continue;
-            }System.out.println("Element " + array[i].city + " was found.");
-            i++;
-        } while (i < array.length);
 
     }
 
